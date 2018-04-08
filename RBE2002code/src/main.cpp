a1631d0f491b4dafb7ab01f720c979809ddaa5d2
Caleb Wagner <cwagner@extranet-cntr-130-215-176-113.vpn.wpi.net>
2018-04-08 10:19:32 -0400

    Added LCD printing and basic higher level control, all states should be added to enum statement

--- RBE2002code/src/main.cpp
+++ RBE2002code/src/main.cpp
@@ -4,18 +4,32 @@
 #include "drive.h"
 #include "Ultrasonic.h"
 #include "globalPins.h"
+#include "PID.h"
+#include <LiquidCrystal.h>
+
+
+//State diagram control
+enum State {STOP, WALLFOLLOW} state;
+float x;
+float y;
+
+
+//Function prototypes
+void followWall();
+void printLCD(int, int, char[]);
 
 //Object Creation
 FireSensor fireSensor;
 drive driveTrain;
-
 Ultrasonic backLeftUltra(BACKLEFTULTRATRIG, BACKLEFTULTRAECHO);
 Ultrasonic frontLeftUltra(FRONTLEFTULTRATRIG, FRONTLEFTULTRAECHO);
-
+Ultrasonic frontUltra(FRONTULTRATRIG, FRONTULTRAECHO);
+PID driveStraightPID;
+LiquidCrystal lcd(40, 41, 42, 43, 44, 45);
 //Still have to implement the revesre part
-//Motor(digitalPin,analogPin,isReverse);
-// Motor leftMotor(29,7,true);
-// Motor rightMotor(28,6,false);
+// Motor(digitalPin,analogPin,isReverse);
+//  Motor leftMotor(29,7,true);
+//  Motor rightMotor(28,6,false);
 
 void setup() {
     //Fire Sensor
@@ -24,15 +38,26 @@ void setup() {
     // pinMode(6,OUTPUT);
     // pinMode(7, OUTPUT);
     fireSensor.initialize(); //this initializes the fire sensor
-    // leftMotor.initialize();
-    // rightMotor.initialize();
+     // leftMotor.initialize();
+     // rightMotor.initialize();
     driveTrain.initialize();
+    driveStraightPID.setpid(5,.1,0);
+
+    lcd.begin(16, 2);
     Serial.begin(9600);
-
-
 }
 
 void loop() {
+    state = WALLFOLLOW;
+    switch(state){
+      case WALLFOLLOW:
+        drive();
+        break;
+      case STOP:
+        driveTrain.setPower(0, 0);
+        break;
+    }
+
     //Fire Sensor hey tye something
     //fireSensor.useSensor();
     //fireSensor.showAll();
@@ -53,7 +78,62 @@ void loop() {
   // rightMotor.setPower(255);
   //
   //driveTrain.setPower(-100,100);
-  frontLeftUltra.readDistance();
-  //backLeftUltra.readDistance();
-  //  leftDrive.setPower(50);
+
+
+}
+
+
+//This function is the state level control for driving
+void drive(){
+  if (frontUltra.readDistance() <= 10){ //the trig and echo pin need to be set to correct values
+    driveTrain.setPower(0, 0);
+    //change to new switch case here, will need to turn now
+  }
+  if (0){ //have this if statement be if line follower is triggered
+
+  }
+  if(0){ //have this if statement be if flame sensor is triggered
+
+  }
+  else{
+    followWall();
+  }
+}
+
+
+//This function has the robot follow a wall using the PID
+void followWall(){
+  int baseRightSpeed = 90;
+  int baseLeftSpeed = 90;
+
+  int frontUltraVal = frontLeftUltra.readDistance();
+  int backUltraVal = backLeftUltra.readDistance();
+
+  float proportionalVal = driveStraightPID.calc(frontUltraVal, backUltraVal);
+
+  //float proportionalValRight = driveStraightPID.calc(12, frontUltraVal);
+  //float proportionalValLeft = driveStraightPID.calc(12, backUltraVal);
+
+  int newLeftSpeed = baseLeftSpeed + proportionalVal;
+  int newRightSpeed = baseRightSpeed - proportionalVal;
+  // leftDrive.setPower(50);
+  Serial.print("Left: ");
+  Serial.println(newLeftSpeed);
+  Serial.print("Right: ");
+  Serial.println(newRightSpeed);
+
+  char message[] = "Left/Right Motor Speeds";
+  printLCD(newLeftSpeed, newRightSpeed, message);
+
+  driveTrain.setPower(newLeftSpeed, newRightSpeed);
+}
+
+void printLCD(int valOne, int valTwo, char message[]){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(valOne);
+  lcd.setCursor(10, 0);
+  lcd.print(valTwo);
+  lcd.setCursor(0,1);
+  lcd.print(message);
 }
