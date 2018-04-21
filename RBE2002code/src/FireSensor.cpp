a9c70b6516ee309c53b336866742300a32881887
Ryan <ryeastwood@gmail.com>
2018-04-21 18:11:23 -0400

    PID tweaks for centering fan

--- RBE2002code/src/FireSensor.cpp
+++ RBE2002code/src/FireSensor.cpp
@@ -60,7 +60,7 @@ void FireSensor::initialize(){
 
   //centerFan.setpid(0.03,0.001,0); //Work good but slow
   //centerFan.setpid(0.03,0.006,0); //Better when working alone
-  centerFan.setpid(0.02,0.004,0); //REVIEW: A lot better
+  centerFan.setpid(0.02,0.004,0.0001); //REVIEW: A lot better
 }
 
 
