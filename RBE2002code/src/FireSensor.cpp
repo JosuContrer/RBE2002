b189a35ca37d3b6a2151d6a11c3861c4f6055243
Ryan <ryeastwood@gmail.com>
2018-04-21 18:03:00 -0400

    firre sensor limits

--- RBE2002code/src/FireSensor.cpp
+++ RBE2002code/src/FireSensor.cpp
@@ -178,6 +178,12 @@ bool FireSensor::centerHeight(){
   if(isFire()){
     int fanError =  centerFan.calc(CENTER_VAL, getz()); //Use PID to center flame
     int newError = STARTVAL - fanError;
+    if (newError>178){
+      newError=178;
+    }
+    if(newError<2){
+      newError=0;
+    }
     fanServo.write(newError); //add error to initial starting position
     //Serial.println(fanError);
     if(fanError <= 2){
